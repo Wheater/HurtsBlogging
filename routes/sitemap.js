8a31d8919f3b902f337f7ff50efb8985b9a57549
unknown <EHurt@LAP14187.int.cardonhealthcare.com>
2015-08-28 10:28:50 -0500

    Added automated sitemap updates

--- routes/sitemap.js
+++ routes/sitemap.js
@@ -7,4 +7,47 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/api/addSitemapEntry/:type/:id', function(req, res, next) {
   
+  var type = req.params.type;
+  var id = req.params.id;  
+
+  fs.readFile('', function(err, data){
+  	if (err){
+  	  console.log(err);
+  	  return err;
+  	}
+
+  	var index = data.indexOf("<\\urlset>");
+  	var newData = data.splice(0, index) + getUrlEntry(type, id) + data.splice(index);
+
+  	fs.writeFile('/app/sitemap.xml', newData, function (err) {
+	  if (err) 
+	  	console.log(err);
+	  console.log('It\'s saved!');
+	});
+  });
+
+  function getFormattedDate(){
+  	var today = new Date();
+  	var dateString = today.getFullYear() + '-' + 
+	  				   today.getMonth() + 1 + '-' + 
+	  				   today.getDate() + '-' +
+	  				   'T' + getHours() + ':' +
+	  				   today.getMinutes() + ':' + 
+	  				   today.getSeconds() + '+00:00';
+	console.log(dateString);
+    return dateString;
+  }
+
+  function getUrlEntry(type, id){
+  	var baseUrl = 'http://hurtsblogging.com';
+
+  	if(type == 'singlePost'){
+  		var fullUrl = baseUrl + '/views/singlePost/' + id;
+  	}
+
+  	var newEntry = '<url><loc>' + fullUrl + '<\\loc><lastmod>' + getFormattedDate() + '<\\lastmod><\\url>'
+  	console.log(newEntry);
+  	return newEntry;
+  }
+
 });
